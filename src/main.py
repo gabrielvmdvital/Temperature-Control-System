997430cae5541e2473e1644d5cfeebef7b002a1c
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-10-21 22:58:23 -0300

    correction in main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
 import json
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 with open(r"src\config\config.json", encoding='utf-8') as fp:
     data = json.load(fp)
 # Press the green button in the gutter to run the script.
