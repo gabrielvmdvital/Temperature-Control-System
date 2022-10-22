135f44bb73cbe39b40bd74eb03294761ed8eb555
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-10-21 23:02:52 -0300

    modifing constructor in Simulator class

--- src/simulator.py
+++ src/simulator.py
@@ -4,12 +4,8 @@ import numpy as np
 class Simulator():
     def __init__(self, nEnvironment, matrixA = None, matrixB = None) -> None:
         self._nEnvironment = nEnvironment
-        if matrixA is None and matrixB is None:
-            self._matrixA = np.array()
-            self._matrixB = np.array()
-        else:
-            self._matrixA = matrixA
-            self._matrixB = matrixB
+        self._matrixA = np.array() if matrixA is None else matrixA
+        self._matrixB = np.array() if matrixB is None else matrixB
 
     @property
     def nEnvironment(self):
