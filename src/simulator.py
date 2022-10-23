322ad11c9710b3dca094713bba2b3dd69a0ab817
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-10-23 16:05:30 -0300

    Adding private method to generate a matrix P

--- src/simulator.py
+++ src/simulator.py
@@ -2,11 +2,21 @@ import numpy as np
 
 
 class Simulator():
-    def __init__(self, nEnvironment: int, interval: tuple =(0, 10),matrixA: np.ndarray = None, matrixB: np.ndarray = None) -> None:
+    def __init__(self, nEnvironment: int, interval: tuple =(0, 10),matrixA: np.ndarray = None, matrixB: np.ndarray = None, matrixP: np.ndarray = None) -> None:
+        """This method is the constructor of the Simulator class
+
+        Args: nEnvironment: Integer value
+              interval: Tuple with integer values
+              matrixA: Environment relationship matrix with float valus
+              matrixB: Influence matrix of actuators in the environment
+
+        Return: None
+        """
         self._nEnvironment = nEnvironment
         self.l, self.h = interval
         self._matrixA = self.__generate_matrixA_values() if matrixA is None else matrixA
         self._matrixB = self.__generate_matrixB_values() if matrixB is None else matrixB
+        self._matrixP = self.__generate_matrixP_values() if matrixP is None else matrixP
 
     @property
     def nEnvironment(self):
@@ -31,6 +41,14 @@ class Simulator():
         Return: Matrix B        
         """
         return self._matrixB
+
+    @property
+    def matrixP(self):
+        """This method is a property used to return the matrix P        
+        Args: None
+        Return: Matrix P       
+        """
+        return self._matrixP
         
     def __generate_matrixA_values(self):
         """This method is used to initialize matrix A with random values in the range between [0,1]      
@@ -40,10 +58,19 @@ class Simulator():
         return np.random.rand(self._nEnvironment, self._nEnvironment)
 
     def __generate_matrixB_values(self):
+        """This method is used to initialize diagonal matrix B representing the nth interaction 
+        between the environment and the actuator.
+        Args: None
+        Return: diagonal np.ndarray with dimensions (nEnvironment x nEnvironment)      
+        """
+        return np.eye(self._nEnvironment, dtype=float)
+       
+
+    def __generate_matrixP_values(self):
         """This method is used to initialize matrix A with random values in the range between [l,h],
         l and h have default values of 0 and 10 respectively.
         Args: None
-        Return: np.ndarray with dimensions (nEnvironment x nEnvironment) which represents the matrix B      
+        Return: np.ndarray with dimensions (nEnvironment x nEnvironment) which represents the matrix B     
         """
         random_matrix_float = np.random.rand(self._nEnvironment, self._nEnvironment)
         random_matriz_int = np.random.randint(low=self.l, high=self.h, size=(self._nEnvironment, self._nEnvironment))
