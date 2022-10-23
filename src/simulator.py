11e3311a9d1e7bef9b6dcf29f96bee6da0aac29e
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-10-23 17:28:05 -0300

    refactoring a methods in Simulator class

--- src/simulator.py
+++ src/simulator.py
@@ -2,7 +2,9 @@ import numpy as np
 
 
 class Simulator():
-    def __init__(self, nEnvironment: int, interval: tuple =(0, 10),matrixA: np.ndarray = None, matrixB: np.ndarray = None, matrixP: np.ndarray = None) -> None:
+    def __init__(self, nEnvironment: int, interval: tuple =(0, 10), matrixA: np.ndarray = None, 
+                 matrixB: np.ndarray = None, matrixP: np.ndarray = None, arrayT: np.ndarray = None, 
+                 arrayU: np.ndarray = None) -> None:
         """This method is the constructor of the Simulator class
 
         Args: nEnvironment: Integer value
@@ -12,11 +14,14 @@ class Simulator():
 
         Return: None
         """
-        self._nEnvironment = nEnvironment
+        self.__nEnvironment = nEnvironment
         self.l, self.h = interval
-        self._matrixA = self.__generate_matrixA_values() if matrixA is None else matrixA
-        self._matrixB = self.__generate_matrixB_values() if matrixB is None else matrixB
-        self._matrixP = self.__generate_matrixP_values() if matrixP is None else matrixP
+        self.__matrixA = self.__generate_matrixA_values() if matrixA is None else matrixA
+        self.__matrixB = self.__generate_matrixB_values() if matrixB is None else matrixB
+        self.__matrixP = self.__generate_matrixP_values() if matrixP is None else matrixP
+        self.__arrayT = self.__generate_arrayT_values() if matrixP is None else arrayT
+        self.__arrayU = np.zeros((self.__nEnvironment, self.__nEnvironment)) if matrixP is None else arrayU
+        self.__memory = [np.dstack((self.__arrayT, self.__arrayU))]
 
     @property
     def nEnvironment(self):
@@ -24,7 +29,7 @@ class Simulator():
         Args: None
         Return: number of environments        
         """        
-        return self._nEnvironment
+        return self.__nEnvironment
 
     @property
     def matrixA(self):
@@ -32,7 +37,7 @@ class Simulator():
         Args: None
         Return: Matrix A    
         """
-        return self._matrixA
+        return self.__matrixA
     
     @property
     def matrixB(self):
@@ -40,7 +45,7 @@ class Simulator():
         Args: None
         Return: Matrix B        
         """
-        return self._matrixB
+        return self.__matrixB
 
     @property
     def matrixP(self):
@@ -48,14 +53,30 @@ class Simulator():
         Args: None
         Return: Matrix P       
         """
-        return self._matrixP
+        return self.__matrixP
+    
+    @property
+    def matrixU(self):
+        """This method is a property used to return the array U        
+        Args: None
+        Return: Matrix P       
+        """
+        return self.__arrayU
+
+    @property
+    def matrixT(self):
+        """This method is a property used to return the array T    
+        Args: None
+        Return: Matrix P       
+        """
+        return self.__arrayT
         
     def __generate_matrixA_values(self):
         """This method is used to initialize matrix A with random values in the range between [0,1]      
         Args: None
         Return: np.ndarray with dimensions (nEnvironment x nEnvironment) which represents the matrix A      
         """
-        return np.random.rand(self._nEnvironment, self._nEnvironment)
+        return np.random.rand(self.__nEnvironment, self.__nEnvironment)
 
     def __generate_matrixB_values(self):
         """This method is used to initialize diagonal matrix B representing the nth interaction 
@@ -63,32 +84,51 @@ class Simulator():
         Args: None
         Return: diagonal np.ndarray with dimensions (nEnvironment x nEnvironment)      
         """
-        return np.eye(self._nEnvironment, dtype=float)
+        return np.eye(self.__nEnvironment, dtype=float)
        
 
     def __generate_matrixP_values(self):
         """This method is used to initialize matrix A with random values in the range between [l,h],
         l and h have default values of 0 and 10 respectively.
         Args: None
-        Return: np.ndarray with dimensions (nEnvironment x nEnvironment) which represents the matrix B     
+        Return: np.ndarray with dimensions (nEnvironment x nEnvironment) which represents the matrix P     
         """
-        random_matrix_float = np.random.rand(self._nEnvironment, self._nEnvironment)
-        random_matriz_int = np.random.randint(low=self.l, high=self.h, size=(self._nEnvironment, self._nEnvironment))
+        random_matrix_float = np.random.rand(self.__nEnvironment, self.__nEnvironment)
+        random_matriz_int = np.random.randint(low=self.l, high=self.h, size=(self.__nEnvironment, self.__nEnvironment))
         return random_matrix_float - random_matriz_int
 
-    def update_matrixA(self):
-        """This method is used to update the values of the matrix
+    def __generate_arrayT_values(self):
+        """This method is used to initialize array T with random values in the range between [15,35].
         Args: None
-        Return: matrix A with updated values      
+        Return: np.ndarray with dimensions (nEnvironment) which represents the array of Temperature      
         """
-        pass
+        random_matrix_float = np.random.rand(self.__nEnvironment, self.__nEnvironment)
+        random_matriz_int = np.random.randint(low=15, high=35, size=self.__nEnvironment)
+        return random_matriz_int - random_matrix_float
 
-    def update_matrixB(self):
-        """This method is used to update the values of the matrix
+    def update_arrayT(self):
+        """This method is used to update the values of the array of Temperature
         Args: None
-        Return: matrix B with updated values      
+        Return: matrix T with updated values      
         """
-        pass
+        return self.__arrayT + 2
+        #pass
+
+    def update_arrayU(self):
+        """This method is used to update the values of the array of Potency
+        Args: None
+        Return: matrix U with updated values      
+        """
+        return self.__arrayU + 1
+        #pass
+
+    def update_memory_list(self):
+        new_values = np.dstack((self.update_arrayT(), self.update_arrayU()))
+        print(new_values)
+        return self.__memory.append(new_values)
+
+    def post_status_nEnvironment(self):            
+        return self.__memory[-1]
 
     def setup(self):
         while True:
