1db6755974231cbed4b1e267308aa601d7da25d9
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-10-23 15:06:38 -0300

    adding  methods to generate random matrix in Simulator class

--- src/simulator.py
+++ src/simulator.py
@@ -4,7 +4,7 @@ import numpy as np
 class Simulator():
     def __init__(self, nEnvironment: int, interval: tuple =(0, 10),matrixA: np.ndarray = None, matrixB: np.ndarray = None) -> None:
         self._nEnvironment = nEnvironment
-        self.low, self.high = interval
+        self.l, self.h = interval
         self._matrixA = self.generate_matrixA_values() if matrixA is None else matrixA
         self._matrixB = self.generate_matrixB_values() if matrixB is None else matrixB
 
@@ -15,9 +15,9 @@ class Simulator():
     def generate_matrixA_values(self):
         return np.random.rand(self._nEnvironment, self._nEnvironment)
 
-    def generate_matrixB_values(self, low, hight):
+    def generate_matrixB_values(self):
         random_matrix_float = np.random.rand(self._nEnvironment, self._nEnvironment)
-        random_matriz_int = np.random.randint(self.low, self.high, size=(self._nEnvironment, self._nEnvironment))
+        random_matriz_int = np.random.randint(low=self.l, high=self.h, size=(self._nEnvironment, self._nEnvironment))
         return random_matrix_float - random_matriz_int
 
 
