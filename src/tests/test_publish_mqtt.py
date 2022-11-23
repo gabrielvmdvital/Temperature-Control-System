bf888ca0d564e861d1ec6014002fc419dc9c5236
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-11-23 01:53:32 -0300

    Revert "Adding directory tests and testes.py file"
    This reverts commit a76fb61771a357cd53199f3a462810594c321245.

--- src/tests/test_publish_mqtt.py
+++ src/tests/test_publish_mqtt.py
@@ -14,21 +14,19 @@ def run():
     if client.connect("localhost", 1883, 60) != 0:
         print("Could not connect to MQTT Broker!")
         sys.exit(-1)
-    timeCount = 0
-    iteration = 1
+    count = 0
     while True:
-        print(f"iteração: {iteration}")
+        print(f"iteração: {count+1}")
         lst = []
-        if timeCount == 10:
+        if count == 10:
             lst = np.array(lst)
             conect_mqtt.publish(client=client, type_data="Temperatura", data_values=lst)
-            timeCount = 0
+            count = 0
         env1.append(random.randint(15, 28))
         env2.append(random.randint(15, 28))
         env3.append(random.randint(15, 28))
         lst = [env1, env2, env3]
-        timeCount += 1
-        iteration += 1
+        count += 1
         time.sleep(1)
         
         
