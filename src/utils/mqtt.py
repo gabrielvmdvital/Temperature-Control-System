f8f1827370e092ef43e37ee7737652703690f0ab
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-11-28 15:16:18 -0300

    added file mqtt.py in utils

---
+++ src/utils/mqtt.py
@@ -0,0 +1,17 @@
+import numpy as np
+import paho.mqtt.publish as publish
+import paho.mqtt.subscribe as subscribe
+
+
+def controlCenter_onMessage():
+    pass
+
+def simulator_onMessage():
+    pass
+
+def publish_tago():
+    pass
+
+
+def subscribe_mosquitto():
+    pass
