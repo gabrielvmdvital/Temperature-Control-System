b27db7e9dd8973643807b0b78779eaa6ca0cb1ee
gabrielvmdvital <gabrielvmdovital@gmail.com>
2022-11-27 23:49:53 -0300

    removed http_conect.py and ar_condicionado.py files

--- src/utils/ar_condicionado.py
+++
@@ -1,7 +0,0 @@
-class ArCondicionado():
-    def __init__(self, temperatura_limite):
-        self.temperatura_limite = temperatura_limite
-
-
-    def check_temp(self):
-        return self.temperatura
